02718d22bbfe287084b1dd721714ab8aa83f3b6d
vincentmpalma <vincentmichaelpalma@gmail.com>
2024-04-19 03:14:39 -0700

    Added theme, display user (not fully functional), and logout.

--- app/src/main/java/com/example/sportsapp/MainActivity.java
+++ app/src/main/java/com/example/sportsapp/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.sportsapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.sportsapp.database.SportsAppRepository;
 import com.example.sportsapp.database.entities.SportsApp;
+import com.example.sportsapp.database.entities.User;
 import com.example.sportsapp.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
@@ -32,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     //TODO: add login information
-    int loggedInUserId = -1; //default user
+    private int loggedInUserId = -1; //default user
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +48,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        loginUser();
+        loginUser(); //make sure user is logged in and we have user object
 
-        if(loggedInUserId == -1){
+        invalidateOptionsMenu();
+
+        if (loggedInUserId == -1) {
             Intent intent = LoginActivity.loginIntentFactory(getApplicationContext()); //getting login intent
             startActivity(intent); //starting login view
         }
@@ -73,13 +83,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loginUser() {
-        //TODO: create login method
+        //TODO: Make login method FUNCTIONAL
+        user = new User("Drew","password");
         loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
     }
 
-    static Intent mainActivityIntentFactory(Context context, int userId){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.logoutMenuItem);
+        item.setVisible(true);
+        item.setTitle(user.getUsername()); //what is displayed in the menu
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                showLogoutDialog();
+                return false;
+            }
+        });
+        return true;
+    }
+
+    private void showLogoutDialog(){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+
+        alertDialog.setMessage("Logout?");
+
+        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertBuilder.create().show();
+    }
+
+    private void logout() {
+        //TODO: Finish logout method
+        startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId) {
         Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra(MAIN_ACTIVITY_USER_ID ,userId);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
         return intent; //returning intent to start mainActivity
     }
 
