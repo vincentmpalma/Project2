681a03cd5f3108d328c543fe52070394e1999057
vincentmpalma <vincentmichaelpalma@gmail.com>
2024-04-28 06:30:47 -0700

    Most of MLB activity scores page is done.

--- app/src/main/java/com/example/sportsapp/MlbActivity.java
+++ app/src/main/java/com/example/sportsapp/MlbActivity.java
@@ -5,9 +5,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -20,9 +24,19 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.LiveData;
 
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.sportsapp.database.SportsAppRepository;
 import com.example.sportsapp.database.entities.User;
 import com.example.sportsapp.databinding.ActivityMlbBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MlbActivity extends AppCompatActivity {
 
@@ -35,6 +49,9 @@ public class MlbActivity extends AppCompatActivity {
     com.example.sportsapp.databinding.ActivityMlbBinding binding;
     private User user;
 
+    TextView data;
+    String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +61,144 @@ public class MlbActivity extends AppCompatActivity {
         repository = SportsAppRepository.getRepository(getApplication());
         loginUser(savedInstanceState);
 
+
+        loadData();
     }
+
+    private void loadData() {
+        LinearLayout myRoot = (LinearLayout) findViewById(R.id.my_layout);
+        LinearLayout a = new LinearLayout(this);
+        a.setOrientation(LinearLayout.HORIZONTAL);
+
+
+
+
+        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(175, 175);
+        LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+
+        String url = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray eventsArray = response.getJSONArray("events");
+                    StringBuilder resultBuilder = new StringBuilder();
+
+                    for (int i = 0; i < eventsArray.length(); i++) {
+                        JSONObject eventObject = eventsArray.getJSONObject(i);
+                        JSONArray competitionsArray = eventObject.getJSONArray("competitions");
+
+                        for (int j = 0; j < competitionsArray.length(); j++) {
+                            JSONObject competition = competitionsArray.getJSONObject(j);
+                            JSONArray competitorsArray = competition.getJSONArray("competitors");
+
+
+                            JSONObject homeTeam = competitorsArray.getJSONObject(0);
+                            JSONObject awayTeam = competitorsArray.getJSONObject(1);
+
+                            JSONObject homeTeamJSONObject = homeTeam.getJSONObject("team");
+                            JSONObject awayTeamJSONObject = awayTeam.getJSONObject("team");
+
+
+                            String homeDisplayName = homeTeamJSONObject.getString("displayName");
+                            String awayDisplayName = awayTeamJSONObject.getString("displayName");
+
+                            String homeScore = homeTeam.getString("score");
+                            String awayScore = awayTeam.getString("score");
+
+                            JSONObject status = competition.getJSONObject("status");
+                            JSONObject type = status.getJSONObject("type");
+                            boolean isCompleted = type.getBoolean("completed");
+                            int inning = status.getInt("period");
+
+                            String homeLogoUrl = homeTeamJSONObject.getString("logo"); // Assuming 'logo' is the key for the logo URL
+                            String awayLogoUrl = awayTeamJSONObject.getString("logo");
+
+                            ImageView homeImageView = new ImageView(getApplicationContext());
+                            homeImageView.setLayoutParams(imageParams);
+
+
+                            ImageView roadImageView = new ImageView(getApplicationContext());
+                            roadImageView.setLayoutParams(imageParams);
+
+                            Glide.with(getApplicationContext()).load(homeLogoUrl).into(homeImageView);
+                            Glide.with(getApplicationContext()).load(awayLogoUrl).into(roadImageView);
+
+                            StringBuilder resultString = new StringBuilder();
+
+                            if (isCompleted) {
+                                resultString.append("FINAL").append("\n");
+                            } else {
+                                resultString.append("Inning: ").append(inning).append("\n");
+                            }
+
+                            StringBuilder gameTitleString = new StringBuilder(homeDisplayName + " vs " + awayDisplayName);
+
+                            TextView gameTitleTextView = new TextView(getApplicationContext());
+                            gameTitleTextView.setLayoutParams(textParams);
+                            gameTitleTextView.setTextSize(15);
+                            gameTitleTextView.setGravity(Gravity.CENTER);
+                            gameTitleTextView.setText(gameTitleString);
+
+
+                            TextView textView = new TextView(getApplicationContext());
+                            textView.setLayoutParams(textParams);
+                            textView.setTextSize(25);
+                            textView.setText(homeScore + "     " + awayScore);
+
+
+                            LinearLayout gameTitleLinearLayout = new LinearLayout(getApplicationContext());
+                            gameTitleLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+                            gameTitleLinearLayout.setGravity(Gravity.CENTER);
+                            gameTitleLinearLayout.addView(gameTitleTextView);
+
+                            LinearLayout linearLayout = new LinearLayout(getApplicationContext());
+                            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+                            linearLayout.setGravity(Gravity.CENTER); // Set gravity to center
+                            linearLayout.addView(homeImageView);
+                            linearLayout.addView(textView);
+                            linearLayout.addView(roadImageView);
+
+                            TextView resultTextView = new TextView(getApplicationContext());
+                            resultTextView.setLayoutParams(textParams);
+                            resultTextView.setTextSize(25);
+                            resultTextView.setText(resultString);
+
+                            LinearLayout resultLinearLayout = new LinearLayout(getApplicationContext());
+                            resultLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+                            resultLinearLayout.setGravity(Gravity.CENTER);
+                            resultLinearLayout.addView(resultTextView);
+
+
+
+                            binding.myLayout.addView(gameTitleLinearLayout);
+                            binding.myLayout.addView(linearLayout);
+                            binding.myLayout.addView(resultLinearLayout);
+
+                        }
+
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //movieData.setValue("Error occurred.");
+            }
+        });
+
+        Volley.newRequestQueue(this).add(request);
+
+    }
+
 
     static Intent mlbIntentFactory(Context context, int userId) {
         Intent intent = new Intent(context, MlbActivity.class);
