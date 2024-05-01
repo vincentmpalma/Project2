e334e5be9dfd46d6abf6faedc44d7b98cfe358dd
gusjavi <gujavier@csumb.edu>
2024-05-01 12:08:12 -0700

    back option now added to guest

--- app/src/main/java/com/example/sportsapp/SearchActivity.java
+++ app/src/main/java/com/example/sportsapp/SearchActivity.java
@@ -2,11 +2,17 @@ package com.example.sportsapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -45,5 +51,45 @@ public class SearchActivity extends AppCompatActivity {
 
     static Intent searchIntentFactory(Context context) {
         return new Intent(context, SearchActivity.class);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.backup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.backupItem);
+        item.setVisible(true);
+
+        item.setTitle("BACK"); //what is displayed in the menu
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                showLogoutDialog();
+                return false;
+            }
+        });
+        return true;
+    }
+
+    private void showLogoutDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(SearchActivity.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+
+        alertBuilder.setMessage("Back?");
+        alertBuilder.setPositiveButton("Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                backup();
+            }
+        });
+
+        alertBuilder.create().show();
+    }
+    private void backup() {
+        startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
     }
 }
